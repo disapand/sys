48cae6100f008952f153645a1585700a01a7f61c
deced <shadow724156286@hotmail.com>
2018-04-13 23:26:31 +0800

    添加联系人信息界面布局完成
    图片上传的问题没解决

--- resources/views/jkr/create.blade.php
+++ resources/views/jkr/create.blade.php
@@ -14,7 +14,7 @@
                                 <span>基本信息</span>
                             </div>
 
-                            <el-form ref="jbxx" label-width="80px" :model="jbxx">
+                            <el-form ref="jbxx" label-width="100px" :model="jbxx">
                                 {{ csrf_field() }}
 
                                 <el-form-item label="姓名">
@@ -29,6 +29,41 @@
                                     <el-input v-model="jbxx.IDCard"></el-input>
                                 </el-form-item>
 
+                                <el-form-item label="性别">
+                                    <el-radio v-model="jbxx.sex" label="男">男</el-radio>
+                                    <el-radio v-model="jbxx.sex" label="女">女</el-radio>
+                                </el-form-item>
+
+                                <el-form-item label="借款类别">
+                                    <el-select v-model="jbxx.jklb" placeholder="请选择借款类别">
+                                        <el-option label="白领贷" value="白领贷"></el-option>
+                                        <el-option label="房贷" value="房贷"></el-option>
+                                        <el-option label="信用贷" value="信用贷"></el-option>
+                                        <el-option label="精英贷" value="精英贷"></el-option>
+                                        <el-option label="房产二次抵押贷" value="房产二次抵押贷"></el-option>
+                                        <el-option label="全款房抵押贷" value="全款房抵押贷"></el-option>
+                                        <el-option label="税贷" value="税贷"></el-option>
+                                    </el-select>
+                                </el-form-item>
+
+                                <el-form-item label="学历">
+                                    <el-select v-model="jbxx.xl" placeholder="请选择学历">
+                                        <el-option label="博士" value="博士"></el-option>
+                                        <el-option label="硕士" value="硕士"></el-option>
+                                        <el-option label="本科" value="本科"></el-option>
+                                        <el-option label="大专" value="大专"></el-option>
+                                        <el-option label="其他" value="其他"></el-option>
+                                    </el-select>
+                                </el-form-item>
+
+                                <el-form-item label="户籍所在地">
+                                    <el-input v-model="jbxx.hj"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="住宅地址">
+                                    <el-input v-model="jbxx.addr"></el-input>
+                                </el-form-item>
+
                                 <el-form-item>
                                     <el-button type="primary" @click="onSubmitPost('{{ route('jbxxCreate') }}', jbxx)">确认提交</el-button>
                                 </el-form-item>
@@ -39,16 +74,253 @@
                         </el-card>
                     </el-col>
                 </el-row>
-
             </el-tab-pane>
-            <el-tab-pane label="配置管理" name="second">
 
+            <el-tab-pane label="职业信息" name="second">
+                <el-row :gutter="10">
+                    <el-col :span="12" :offset="6">
+                        <el-card class="box-card">
+                            <div slot="header" class="clearfix">
+                                <span>职业信息</span>
+                            </div>
+
+                            <el-form ref="jbxx" label-width="100px" :model="zyxx">
+                                {{ csrf_field() }}
+
+                                <el-form-item label="工作单位">
+                                    <el-input v-model="zyxx.gzdw"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="单位性质">
+                                    <el-input v-model="zyxx.dwxz"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="所属行业">
+                                    <el-input v-model="zyxx.sshy"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="任职部门">
+                                    <el-input v-model="zyxx.rzbm"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="职位">
+                                    <el-input v-model="zyxx.zw"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="入职时间">
+                                    <el-date-picker
+                                            v-model="zyxx.rzsj"
+                                            type="date"
+                                            placeholder="选择日期">
+                                    </el-date-picker>
+                                </el-form-item>
+
+                                <el-form-item label="单位地址">
+                                    <el-input v-model="zyxx.dwdz"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="单位电话">
+                                    <el-input v-model="zyxx.dwdh"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="任职薪水">
+                                    <el-input v-model="zyxx.rzxs"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="总收入">
+                                    <el-input v-model="zyxx.zsr"></el-input>
+                                </el-form-item>
+
+                                <el-form-item>
+                                    <el-button type="primary" @click="onSubmitPost('{{ route('jbxxCreate') }}', jbxx)">确认提交</el-button>
+                                </el-form-item>
+
+                            </el-form>
+                            @include('shared._message_vue')
+                            @include('shared._errors_vue')
+                        </el-card>
+                    </el-col>
+                </el-row>
             </el-tab-pane>
-            <el-tab-pane label="角色管理" name="third">
 
+            <el-tab-pane label="联系人信息" name="third">
+                <el-row :gutter="10">
+                    <el-col :span="12" :offset="6">
+                        <el-card class="box-card">
+                            <div slot="header" class="clearfix">
+                                <span>联系人信息</span>
+                            </div>
+
+                            <el-form ref="jbxx" label-width="180px" :model="lxrxx">
+                                {{ csrf_field() }}
+
+                                <el-form-item label="联系人姓名">
+                                    <el-input v-model="lxrxx.name"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="和申请人关系">
+                                    <el-select v-model="lxrxx.gx" placeholder="请选择">
+                                        <el-option label="直系亲属" value="直系亲属"></el-option>
+                                        <el-option label="朋友" value="朋友"></el-option>
+                                        <el-option label="同事" value="同事"></el-option>
+                                        <el-option label="其他" value="其他"></el-option>
+                                    </el-select>
+                                </el-form-item>
+
+                                <el-form-item label="联系电话">
+                                    <el-input v-model="lxrxx.lxdh"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="身份证号">
+                                    <el-input v-model="lxrxx.sfzh"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="联系人是否知道此贷款">
+                                    <el-radio v-model="lxrxx.dk" label="是">是</el-radio>
+                                    <el-radio v-model="lxrxx.dk" label="否">否</el-radio>
+                                </el-form-item>
+
+                                <el-form-item>
+                                    <el-button type="primary" @click="onSubmitPost('{{ route('jbxxCreate') }}', jbxx)">确认提交</el-button>
+                                </el-form-item>
+
+                            </el-form>
+                            @include('shared._message_vue')
+                            @include('shared._errors_vue')
+                        </el-card>
+                    </el-col>
+                </el-row>
             </el-tab-pane>
-            <el-tab-pane label="定时任务补偿" name="fourth">
 
+
+            <el-tab-pane label="其他信息" name="fourth">
+                <el-row :gutter="10">
+                    <el-col :span="12" :offset="6">
+                        <el-card class="box-card">
+                            <div slot="header" class="clearfix">
+                                <span>其他信息</span>
+                            </div>
+
+                            <el-form ref="jbxx" label-width="130px" :model="qtxx">
+                                {{ csrf_field() }}
+
+                                <el-form-item label="房产类别">
+                                    <el-input v-model="qtxx.fclb"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="购买时间">
+                                    <el-date-picker
+                                            v-model="qtxx.gmsj"
+                                            type="date"
+                                            placeholder="选择日期">
+                                    </el-date-picker>
+                                </el-form-item>
+
+                                <el-form-item label="购买价格">
+                                    <el-input v-model="qtxx.gmjg"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="购买方式">
+                                    <el-input v-model="qtxx.gmfs"></el-input>
+                                </el-form-item>
+
+                                <el-form-item label="购买地址">
+                                    <el-input v-model="qtxx.gmdz"></el-input>
+                                </el-form-item>
+
+                                <el-form-item>
+                                    <el-button type="primary" @click="onSubmitPost('{{ route('jbxxCreate') }}', jbxx)">确认提交</el-button>
+                                </el-form-item>
+
+                            </el-form>
+                            @include('shared._message_vue')
+                            @include('shared._errors_vue')
+                        </el-card>
+                    </el-col>
+                </el-row>
+            </el-tab-pane>
+
+
+            <el-tab-pane label="附件信息" name="fifth">
+                <el-row :gutter="10">
+                    <el-col :span="12" :offset="6">
+                        <el-card class="box-card">
+                            <div slot="header" class="clearfix">
+                                <span>附件信息</span>
+                            </div>
+
+                            <el-form ref="jbxx" label-width="130px" :model="fjxx">
+                                {{ csrf_field() }}
+
+                                <el-form-item label="身份证正面照片">
+                                    <el-upload
+                                            class="upload-demo"
+                                            action="https://jsonplaceholder.typicode.com/posts/"
+                                            :on-preview="handlePreview"
+                                            ref="upload"
+                                            :on-remove="handleRemove"
+                                            :file-list="fileList2"
+                                            list-type="picture"
+                                            :auto-upload="false">
+                                        <el-button size="small" type="primary">点击添加</el-button>
+                                        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                                    </el-upload>
+                                </el-form-item>
+
+                                <el-form-item label="身份证反面照片">
+                                    <el-upload
+                                            class="upload-demo"
+                                            action="https://jsonplaceholder.typicode.com/posts/"
+                                            :on-preview="handlePreview"
+                                            ref="upload"
+                                            :on-remove="handleRemove"
+                                            :file-list="fileList2"
+                                            list-type="picture"
+                                            :auto-upload="false">
+                                        <el-button size="small" type="primary">点击添加</el-button>
+                                        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                                    </el-upload>
+                                </el-form-item>
+                                <el-form-item label="居住证明照片">
+                                    <el-upload
+                                            class="upload-demo"
+                                            action="https://jsonplaceholder.typicode.com/posts/"
+                                            :on-preview="handlePreview"
+                                            ref="upload"
+                                            :on-remove="handleRemove"
+                                            :file-list="fileList2"
+                                            list-type="picture"
+                                            :auto-upload="false">
+                                        <el-button size="small" type="primary">点击添加</el-button>
+                                        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                                    </el-upload>
+                                </el-form-item>
+
+                                <el-form-item label="车辆信息照片">
+                                    <el-upload
+                                            class="upload-demo"
+                                            action="https://jsonplaceholder.typicode.com/posts/"
+                                            :on-preview="handlePreview"
+                                            ref="upload"
+                                            :on-remove="handleRemove"
+                                            :file-list="fileList2"
+                                            list-type="picture"
+                                            :auto-upload="false">
+                                        <el-button size="small" type="primary">点击添加</el-button>
+                                        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                                    </el-upload>
+                                </el-form-item>
+
+                                <el-form-item>
+                                    <el-button type="primary" @click="onSubmitPost('{{ route('jbxxCreate') }}', jbxx)">点击上传</el-button>
+                                </el-form-item>
+
+                            </el-form>
+                            @include('shared._message_vue')
+                            @include('shared._errors_vue')
+                        </el-card>
+                    </el-col>
+                </el-row>
             </el-tab-pane>
         </el-tabs>
     </template>
--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -3,6 +3,18 @@
 @section('content')
     <el-row :gutter="10">
         <el-col :span="12" :offset="6">
+
+           {{-- <div style="margin-bottom: 20px;">
+                <el-input placeholder="请输入内容" v-model="input5" class="input-with-select">
+                    <el-select v-model="select" slot="prepend" placeholder="请选择" style="width: 130px;">
+                        <el-option label="餐厅名" value="1"></el-option>
+                        <el-option label="订单号" value="2"></el-option>
+                        <el-option label="用户电话" value="3"></el-option>
+                    </el-select>
+                    <el-button slot="append" icon="el-icon-search"></el-button>
+                </el-input>
+            </div>--}}
+
             <el-card class="box-card">
                 <div slot="header" class="clearfix">
                     <span>用户列表</span>
